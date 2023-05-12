03471aa6e95dcf3171abe7b16c02d3a53c6f464a
M-H-Amini <m.hossein.amini@gmail.com>
2023-05-12 12:49:46 -0400

    transformation added to crop dataset...

--- mh_dave2_data.py
+++ mh_dave2_data.py
@@ -34,14 +34,16 @@ def plotHistogram(df, col='steering', show=True):
     plt.xlabel('Steering Angle')
     plt.ylabel('Frequency')
     show and plt.show()
+    plt.close()
 
-def readDataset(dataset_csv, dataset_folder, train_cols):
+def readDataset(dataset_csv, dataset_folder, train_cols, transform=lambda x: x):
     """Gets dataset folder and csv file and returns a dataframe of images and steering angles.
 
     Args:
         dataset_csv (str): Path to csv file.
         dataset_folder (str): Path to dataset folder.
         train_cols (list): List of columns to be read from csv file.
+        transform (function, optional): Transformation function. Defaults to lambda x: x.
 
     Returns:
         df (pandas.DataFrame): Dataframe of images and steering angles.
@@ -50,6 +52,7 @@ def readDataset(dataset_csv, dataset_folder, train_cols):
     df[train_cols] = df[train_cols].applymap(lambda x: os.path.join(dataset_folder, x.split('\\')[-1]))
     df[['center_path', 'left_path', 'right_path']] = df[['center', 'left', 'right']]
     df[train_cols] = df[train_cols].applymap(lambda x: np.asarray(Image.open(x)))
+    df[train_cols] = df[train_cols].applymap(transform)
     return df
 
 def balanceDataset(df, col='steering', reduce_ratio=0.9, show=True):
@@ -84,24 +87,35 @@ def splitDataset(df, train_cols, test_size=0.2, val_size=0.2, random_state=28):
 
     Returns:
         X_train (numpy.ndarray): Train set features.
+        y_train (numpy.ndarray): Train set labels.
+        meta_train (list): Train set metadata.
+        X_val (numpy.ndarray): Validation set features.
+        y_val (numpy.ndarray): Validation set labels.
+        meta_val (list): Validation set metadata.
+        X_test (numpy.ndarray): Test set features.
+        y_test (numpy.ndarray): Test set labels.
+        meta_test (list): Test set metadata.
     """
     train_df, test_df = train_test_split(df, test_size=test_size, random_state=random_state)
-    train_df, valid_df = train_test_split(train_df, test_size=val_size, random_state=random_state)
+    train_df, val_df = train_test_split(train_df, test_size=val_size, random_state=random_state)
     log.info(f'Train dataset size: {len(train_df)}')
-    log.info(f'Valid dataset size: {len(valid_df)}')
+    log.info(f'Val dataset size: {len(val_df)}')
     log.info(f'Test dataset size: {len(test_df)}')
-    X_train, y_train = train_df[train_cols].values, train_df['steering'].values
-    X_valid, y_valid = valid_df[train_cols].values, valid_df['steering'].values
-    X_test, y_test = test_df[train_cols].values, test_df['steering'].values
+    X_train, y_train, meta_train = train_df[train_cols].values, train_df['steering'].values, train_df[[col+'_path' for col in train_cols]].values
+    X_val, y_val, meta_val = val_df[train_cols].values, val_df['steering'].values, val_df[[col+'_path' for col in train_cols]].values
+    X_test, y_test, meta_test = test_df[train_cols].values, test_df['steering'].values, test_df[[col+'_path' for col in train_cols]].values
     X_train = np.array([xx for x in X_train for xx in x])  ##  (N, 160, 320, 3)
     y_train = np.array([y for y in y_train for _ in range(len(train_cols))])  ##  (N, 1)
-    X_valid = np.array([xx for x in X_valid for xx in x])  ##  (N, 160, 320, 3)
-    y_valid = np.array([y for y in y_valid for _ in range(len(train_cols))])  ##  (N, 1)
+    meta_train = [mm for m in meta_train for mm in m]  ##  (N,)
+    X_val = np.array([xx for x in X_val for xx in x])  ##  (N, 160, 320, 3)
+    y_val = np.array([y for y in y_val for _ in range(len(train_cols))])  ##  (N, 1)
+    meta_val = [mm for m in meta_val for mm in m]  ##  (N,)
     X_test = np.array([xx for x in X_test for xx in x])  ##  (N, 160, 320, 3)
     y_test = np.array([y for y in y_test for _ in range(len(train_cols))])  ##  (N, 1)
-    return X_train, y_train, X_valid, y_valid, X_test, y_test
+    meta_test = [mm for m in meta_test for mm in m]  ##  (N,)
+    return X_train, y_train, meta_train, X_val, y_val, meta_val, X_test, y_test, meta_test
 
-def prepareDataset(dataset_folder, dataset_csv, train_cols, reduce_ratio=0.9, test_size=0.2, val_size=0.2, random_state=28, show=True):
+def prepareDataset(dataset_folder, dataset_csv, train_cols, reduce_ratio=0.9, test_size=0.2, val_size=0.2, random_state=28, transform=lambda x:x, show=True):
     """Prepares a dataset for training.
 
     Args:
@@ -112,24 +126,49 @@ def prepareDataset(dataset_folder, dataset_csv, train_cols, reduce_ratio=0.9, te
         test_size (float, optional): Ratio of test set size to the whole dataset. Defaults to 0.2.
         val_size (float, optional): Ratio of validation set size to the train set. Defaults to 0.2.
         random_state (int, optional): Random state. Defaults to 28.
+        transform (function, optional): Transform function. Defaults to lambda x:x.
         show (bool, optional): Whether to show the plot. Defaults to True.
 
     Returns:
         X_train (numpy.ndarray): Train set features.
     """
 
-    df = readDataset(dataset_csv, dataset_folder, train_cols)
+    df = readDataset(dataset_csv, dataset_folder, train_cols, transform=transform)
     df = balanceDataset(df, reduce_ratio=reduce_ratio, show=show)
-    X_train, y_train, X_valid, y_valid, X_test, y_test = splitDataset(df, train_cols, test_size=test_size, val_size=val_size, random_state=random_state)
-    return X_train, y_train, X_valid, y_valid, X_test, y_test
+    X_train, y_train, meta_train, X_val, y_val, meta_val, X_test, y_test, meta_test = splitDataset(df, train_cols, test_size=test_size, val_size=val_size, random_state=random_state)
+    return X_train, y_train, meta_train, X_val, y_val, meta_val, X_test, y_test, meta_test
+
+def validate(X, y, meta, dataset_folder, dataset_csv, n=5, transform=lambda x:x, show=True):
+    df = pd.read_csv(dataset_csv, header=None, names=['center', 'left', 'right', 'steering', 'throttle', 'brake', 'speed'])
+    df[['center', 'left', 'right']] = df[['center', 'left', 'right']].applymap(lambda x: os.path.join(dataset_folder, x.split('\\')[-1]))
+    for i in range(n):
+        index = np.random.randint(len(X))
+        img = X[index]
+        steering = y[index]
+        img_path = meta[index]
+        img_gt = transform(np.asarray(Image.open(img_path)))  ##  Ground truth image
+        steering_gt = df[df.apply(lambda x: img_path in x.values, axis=1)]['steering'].values[0]
+        assert steering == steering_gt, f'Steering angle {steering} is not equal to its version in the dataset.'
+        assert np.all(img == img_gt), f'Image {img_path} is not equal to its version in the dataset.'
+        if show: 
+            plt.figure(figsize=(10, 5))
+            plt.imshow(img)
+            plt.title(f'Steering angle: {steering}\nGround truth steering angle: {steering_gt}\nImage path: {img_path}')
+            plt.show()
+            plt.close()
+    log.info('\033[92m' + 'Validation passed!' + '\033[0m')
+    
 
 
 if __name__  == '__main__':
     ##  Dataset...
     dataset_folder = 'UdacityDS/self_driving_car_dataset_jungle/IMG'
     dataset_csv = 'UdacityDS/self_driving_car_dataset_jungle/driving_log.csv'
-    train_cols = ['center']  ##  ['center', 'left', 'right']
-    X_train, y_train, X_valid, y_valid, X_test, y_test = prepareDataset(dataset_folder, dataset_csv, train_cols)
+    train_cols = ['center']  ##  Or it can be ['center', 'left', 'right']
+    transform = lambda x: x[60:150, :, :]
+    # train_cols = ['center', 'left', 'right']
+    X_train, y_train, meta_train, X_val, y_val, meta_val, X_test, y_test, meta_test = prepareDataset(dataset_folder, dataset_csv, train_cols, transform=transform, show=False)
     log.info(f'X_train shape: {X_train.shape}, y_train shape: {y_train.shape}')
-    log.info(f'X_valid shape: {X_valid.shape}, y_valid shape: {y_valid.shape}')
+    log.info(f'X_val shape: {X_val.shape}, y_val shape: {y_val.shape}')
     log.info(f'X_test shape: {X_test.shape}, y_test shape: {y_test.shape}')
+    validate(X_train, y_train, meta_train, dataset_folder, dataset_csv, n=5, transform=transform, show=True)
