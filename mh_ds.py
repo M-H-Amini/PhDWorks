e06abfc18c44862e733d63e62f6b0edad6026a6f
M-H-Amini <m.hossein.amini@gmail.com>
2023-05-24 18:03:18 -0400

    fake dataset added to mh_ds.py...

--- mh_ds.py
+++ mh_ds.py
@@ -9,6 +9,9 @@ from mh_beamng_ds import prepareDataset as prepareDatasetBeamNG
 from git_dave2_model import loadModel as loadModelGit
 from functools import partial
 import tensorflow_datasets as tfds
+from tqdm import tqdm 
+import cv2
+import os
 
 log.basicConfig(level=log.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
  
@@ -51,13 +54,28 @@ def loadDataset(dataset='udacity'):
         X_test = np.array([x.numpy()/255. for x, y in ds_test])
         y_test = np.array([y.numpy() for x, y in ds_test])
         return X_train, y_train, None, None, X_test, y_test
+    elif dataset == 'fake':
+        folder = '/home/mohammadh/DCLGAN/results/beamng2udacity_SimDCL/test_latest/images/fake_A'
+        transform_b = lambda x: x[130-66:130, 60:260, :]  / 255.
+        X_train = []
+        for i in tqdm(os.listdir(folder)):
+            if i.endswith('.png'):
+                img = cv2.imread(os.path.join(folder, i))[..., ::-1]
+                img = transform_b(img)
+                X_train.append(img)
+        X_train = np.array(X_train)
+        X_test = X_train[-100:]
+        X_train = X_train[:-100]
+        return X_train, None, None, None, X_test, None
+
     
 if __name__ == '__main__':
     # X_train, y_train, X_val, y_val, X_test, y_test = loadDataset('udacity')
     # X_train, y_train, _, _, X_test, y_test = loadDataset('cats_vs_dogs')
     X_train, y_train, _, _, X_test, y_test = loadDataset('mnist')
+    X_train, _, _, _, _, _ = loadDataset('fake')
     print('X_train.shape:', X_train.shape)
-    print('y_train.shape:', y_train.shape)
-    print('X_test.shape:', X_test.shape)
-    print('y_test.shape:', y_test.shape)
+    # print('y_train.shape:', y_train.shape)
+    # print('X_test.shape:', X_test.shape)
+    # print('y_test.shape:', y_test.shape)
 
