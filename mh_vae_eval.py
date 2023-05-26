c2c47ebdcecf009a4484e96af8d94404d5c0364c
M-H-Amini <m.hossein.amini@gmail.com>
2023-05-26 19:10:38 -0400

    mh_vae_eval updated...

--- mh_vae_eval.py
+++ mh_vae_eval.py
@@ -7,25 +7,6 @@ from mh_utils import buildQ, buildP
 import logging as log
 from mh_ds import loadDataset
 
-log.basicConfig(level=log.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-##  Loading data...
-ds_names = ['udacity', 'beamng', 'fake_gan']
-ds = [loadDataset(ds_name) for ds_name in ds_names]
-ds = map(lambda x: np.concatenate((x[0], x[4]), axis=0), ds)
-ds = {ds_name: ds_ for ds_name, ds_ in zip(ds_names, ds)}
-
-##  Building models...
-latent_dim = 20
-
-model_q = buildQ()
-model_p = buildP()
-
-###  MHVAE model...
-model = MHVAE(input_dim=(66, 200, 3), latent_dim=latent_dim, model_p=model_p, model_q=model_q, regularization_const=100000)
-model.compile(optimizer='adam')
-model.load_weights('mh_cvae_weights.h5')
-log.info('\033[92m' + 'Model loaded!' + '\033[0m')
 
 def eval(X, X_hat):
     """Evaluates the reconstruction error.
@@ -39,17 +20,34 @@ def eval(X, X_hat):
     """
     return np.mean(np.square(X - X_hat), axis=(1, 2, 3))
 
-error = [eval(ds[name], model.predict(ds[name])) for name in ds_names]
+def plotEvalHistogram(model, ds:dict, output_name=None):
+    error = [eval(ds[name], model.predict(ds[name])) for name in ds.keys()]
+    plt.figure(figsize=(10, 5))
+    plt.legend()
+    [plt.hist(e, bins=50, label=name, density=True, lw=0) for e, name in zip(error, ds.keys())]
+    plt.title('Reconstruction Error')
+    plt.xlabel('MSE')
+    plt.ylabel('Count')
+    plt.legend()
+    output_name and plt.savefig(output_name) 
+    output_name and f'Histogram saved to {output_name}!'
+    plt.show()
 
-plt.figure(figsize=(10, 5))
-
-plt.legend()
-##  Plotting histograms with x axis in log scale...
-[plt.hist(e, bins=50, label=name, density=True, lw=0) for e, name in zip(error, ds_names)]
-# plt.gca().set_xscale('log')
-plt.title('Reconstruction Error')
-plt.xlabel('MSE')
-plt.ylabel('Count')
-plt.legend()
-plt.savefig('mh_cvae_eval.pdf')
-plt.show()
+if __name__ == '__main__':
+    log.basicConfig(level=log.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    ##  Loading data...
+    ds_names = ['udacity', 'beamng', 'fake_gan']
+    ds = [loadDataset(ds_name) for ds_name in ds_names]
+    ds = map(lambda x: np.concatenate((x[0], x[4]), axis=0), ds)
+    ds = {ds_name: ds_ for ds_name, ds_ in zip(ds_names, ds)}
+    ##  Building models...
+    latent_dim = 20
+    model_q = buildQ()
+    model_p = buildP()
+    ###  MHVAE model...
+    model = MHVAE(input_dim=(66, 200, 3), latent_dim=latent_dim, model_p=model_p, model_q=model_q, regularization_const=100000)
+    model.compile(optimizer='adam')
+    model.load_weights('mh_cvae_weights.h5')
+    log.info('\033[92m' + 'Model loaded!' + '\033[0m')
+    ##  Evaluate...
+    plotEvalHistogram(model, ds, output_name='mh_cvae_eval.pdf')
